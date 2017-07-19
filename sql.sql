7ae17aea517382d6f81ed01434ef8e95dea2d1c8
Eser Sahiner <esershnr@gmail.com>
2017-07-19 09:42:31 +0300

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO `items` (`id`, `libelle`, `isIllegal`, `canUse`) VALUES
+(15, 'Anchovy', 0, 2),
+(16, 'Pompano Fish', 0, 2),
+(17, 'Salmon', 0, 2),
+(18, 'Tuna', 0, 2),
+(19, 'Sardany', 0, 2),
+(20, 'Mullet', 0, 2);
